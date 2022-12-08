c948034ad61dec66e14224b3b3b6c0ce819fc4d3
RaphD <r.durier@hotmail.fr>
2022-12-08 12:17:54 +0100

    ADD: PoeType (enum) + poe Model

---
+++ src/app/core/models/poe.ts
@@ -0,0 +1,41 @@
+export class Poe {
+    private id: number = 0;
+    private title: string = "";
+    private beginDate!: Date;
+    private endDate!: Date;
+    private poeType!: PoeType;
+
+
+    public getId(): number {
+        return this.id;
+    }
+    
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public getBeginDate(): Date {
+        return this.beginDate;
+    }
+
+    public setBirthDate(beginDate: Date): void {
+        this.beginDate = beginDate;
+    }
+
+    public getEndDate(): Date {
+        return this.endDate;
+    }
+
+    public setEndDate(endDate: Date): void {
+        this.endDate = endDate;
+    }
+
+    public getPoeType(): PoeType {
+        return this.poeType;
+    }
+
+    public setPoeType(poeType: PoeType): void {
+        this.poeType = poeType;
+    }
+}
+
