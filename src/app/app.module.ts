c704281cf5e8c11e8171783c1cf1342c514a9b07
RaphD <r.durier@hotmail.fr>
2022-12-08 15:05:32 +0100

    ADD: filter + form (en cours)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { StagiaireFormComponent } from './stagiaires/components/stagiaire-form/s
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { PoeFormComponent } from './core/poes/components/poe-form/poe-form.component';
+import { PoeFilterComponent } from './core/poes/components/poe-filter/poe-filter.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { SharedModule } from './shared/shared.module';
     InitialsPipe,
     StagiaireDetailComponent,
     BubbleDirective,
-    StagiaireFormComponent
+    StagiaireFormComponent,
+    PoeFormComponent,
+    PoeFilterComponent
   ],
   imports: [
     BrowserModule,
