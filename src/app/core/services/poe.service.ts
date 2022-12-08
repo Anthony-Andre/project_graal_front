aef1fe3b77701ce1f96af9d50a68dd9338d4f5ea
Geoffrey <geoffrey.leroy1981@gmail.com>
2022-12-08 12:31:29 +0100

    Avec findAll service

--- src/app/core/services/poe.service.ts
+++ src/app/core/services/poe.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable, take } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Poe } from '../models/poe';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoeService {
 
-  constructor() { }
+  private poes: Array<Poe> = [];
+  private controllerBaseUrl: string = `${environment.apiBaseUrl}/poe`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  public findAll(): Observable<any> {
+    return this.httpClient.get<any>(
+      this.controllerBaseUrl
+    )
+      .pipe(
+        take(1),
+        map((poes: any[]) => {
+          return poes.map((inputPoe: any) => {
+            const poe: Poe = new Poe();
+            poe.setId(inputPoe.id);
+            poe.setTitle(inputPoe.title);
+            poe.setBeginDate(inputPoe.beginDate);
+            poe.setEndDate(inputPoe.endDate);
+            poe.setPoeType(inputPoe.poeType);            
+            return poe;
+          })
+        })
+      )
+  }
 }
