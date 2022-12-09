0091d1208a8f4a6c037c27e0548a35b153fd1f45
Geoffrey <geoffrey.leroy1981@gmail.com>
2022-12-09 14:14:38 +0100

    app routing MAJ 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,7 +43,7 @@ export class AppRoutingModule {
     },
     {
       path: 'stagiaire',
-      component: StagiaireFormComponent
+      component: StagiaireTableComponent
     },
     {
       path: 'stagiaire/add',
