d01e7e93cb5eb081389844a04dcce2b6a38585f6
anthony.andre <anthony-andre@live.fr>
2022-12-09 11:38:44 +0100

    ADD: filter POE

--- src/app/core/poes/components/poe-table/poe-table.component.ts
+++ src/app/core/poes/components/poe-table/poe-table.component.ts
@@ -1,6 +1,4 @@
-import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Poe } from 'src/app/core/models/poe';
 import { PoeService } from 'src/app/core/services/poe.service';
 
@@ -14,9 +12,7 @@ export class PoeTableComponent implements OnInit {
   public poes: Array<Poe> = [];
   public stopDate: String | null = null;
 
-  constructor(
-    private poeService: PoeService,
-    private router: Router) { }
+  constructor(private poeService: PoeService) { }
 
   ngOnInit(): void {
     this.poeService.findAll().subscribe((poes: Poe[]) => {
@@ -26,20 +22,31 @@ export class PoeTableComponent implements OnInit {
 
   public onRemove(poe: Poe): void {
     console.log(`L'utilisateur souhaite supprimer ${poe.getTitle()}`);
-    this.poeService.delete(poe).subscribe({
-      next: (response: HttpResponse<any>) => { },
-      error: (error: any) => { },
-      complete: () => {
-        this.poes.splice(
-          this.poes.findIndex((p: Poe) => p.getId() === poe.getId()), 1
-        )
-      }
-    })
   }
 
   public onUpDate(poe: Poe): void {
     console.log(`L'utilisateur souhaite modifier ${poe.getTitle()}`);
-    this.router.navigate(['/', 'poe', 'update', poe.getId()]);
   }
+
+  public filterChanged(event: String | null): void {
+    console.log(`Filter has changed to : ${event}`);
+    this.stopDate = event;
+  }
+
+  public changeView(poe: Poe): boolean {
+    if (this.stopDate === null) {
+      return true;
+    }
+    if (this.stopDate === "oneMonth") {
+    }
+    return false;
+
+
+
+
+
+  }
+
+
 
 }
