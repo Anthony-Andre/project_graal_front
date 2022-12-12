2e338464f7bb524964bcd8a3865a2c83ab06a6d8
RaphD <r.durier@hotmail.fr>
2022-12-12 14:23:42 +0100

    impl userDto + create userGuard

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { UserDto } from '../dto/user-dto';
+import { User } from '../models/user';
 
 // @TODO: remove after wiring to backend
-const users: any[] = [
+const users: UserDto[] = [
   {
     id: 1,
     login: 'admin',
@@ -20,14 +22,22 @@ const users: any[] = [
 })
 export class UserService {
 
+  private _user: User | null = null;
+
   constructor() { }
 
   public login(formData: any): Observable<boolean> {
-    return of(
-      users.findIndex((user: any) => {
-        return user.login === formData.userLogin && user.password === formData.userPassword
-      }) !== -1
-    )
+      const userIndex: number = users.findIndex((user: UserDto) =>
+      user.login === formData.userLogin && user.password === formData.userPassword);
+      if (userIndex === -1) {
+        return of(false);
+      } else { // so we got a user
+        this._user = new User();
+        this._user.id = users[userIndex].id!;
+        this._user.login = users[userIndex].login;
+
+        return of(true);
+      }
 
     // Other way
     // let found: boolean = false;
@@ -41,4 +51,8 @@ export class UserService {
   }
 
   public logout(): void {}
+
+  public hasUser(): boolean {
+    return this._user !== null;
+  }
 }
