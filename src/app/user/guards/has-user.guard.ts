42666b8276014c5ae5f18864df9e834e1ff29015
anthony.andre <anthony-andre@live.fr>
2022-12-13 09:45:55 +0100

    Implements has user guard

---
+++ src/app/user/guards/has-user.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { timeStamp } from 'console';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HasUserGuard implements CanActivate {
+
+  public constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    // If user : 
+    if (this.userService.hasUser().getValue()) {
+      return true;
+    }
+
+    // If no user => Go to login
+    this.router.navigate(['/', 'login']);
+    return false;
+
+  }
+
+}
